0b026a19adb9cce9eda86786bb4fb6845da4ca29
alice8347 <alice8347@hotmail.com>
2015-08-11 16:00:27 -0400

    Fifth Commit

--- EvilCorpBanking/src/TransactionList.java
+++ EvilCorpBanking/src/TransactionList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class TransactionList {
-	ArrayList<Transaction> transactionList;
+	private ArrayList<Transaction> transactionList;
 	
 	public TransactionList() {
 		transactionList = new ArrayList<Transaction>();
@@ -22,32 +22,32 @@ public class TransactionList {
 	public ArrayList<Transaction> sort() {
 		for (int i = 0; i < transactionList.size(); i++) {
 			for (int j = i; j < transactionList.size(); j++) {
-				// Sort the date first
+				// Sort the date first.
 				if (transactionList.get(i).getDate().compareTo(transactionList.get(j).getDate()) > 0) {
 					Transaction temp3 = transactionList.get(i);
 					transactionList.set(i, transactionList.get(j));
 					transactionList.set(j, temp3);
 				}
 				
-				// If the dates are the same, compare the transaction types and amounts
+				// If the dates are the same, compare the transaction types and amounts.
 				if (transactionList.get(i).getDate().compareTo(transactionList.get(j).getDate()) == 0) {
-					// If the second transaction type is "add an account", swap the order
+					// If the second transaction type is "add an account", swap the order.
 					if (transactionList.get(j).getType().equals("add an account")) {
 						Transaction temp1 = transactionList.get(i);
 						transactionList.set(i, transactionList.get(j));
 						transactionList.set(j, temp1);
 						
-					// If the second transaction type is "deposit" and the first one is any of the other types, keep the same order
+					// If the second transaction type is "deposit" and the first one is any of the other types, keep the same order.
 					} else if ((!transactionList.get(i).getType().equals("deposit")) && transactionList.get(j).getType().equals("deposit")) {
 						continue;
 					
-					// if the first transaction type is "deposit" and the second one is any of the other types, swap the order
+					// if the first transaction type is "deposit" and the second one is any of the other types, swap the order.
 					} else if (transactionList.get(i).getType().equals("deposit") && (!transactionList.get(j).getType().equals("deposit"))){
 						Transaction temp2 = transactionList.get(i);
 						transactionList.set(i, transactionList.get(j));
 						transactionList.set(j, temp2);
 						
-					// If both of the transactions are not "deposit", put the one with larger amount first
+					// If both of the transactions are not "deposit", put the one with larger amount first.
 					} else if (transactionList.get(i).getAmount() < transactionList.get(j).getAmount()) {
 						Transaction temp3 = transactionList.get(i);
 						transactionList.set(i, transactionList.get(j));
