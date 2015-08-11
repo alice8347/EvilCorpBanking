0aafad2c32c4ac1ff84894a472e09643a945093a
alice8347 <alice8347@hotmail.com>
2015-08-11 03:00:53 -0400

    Fourth Commit
    Evil Corp Banking Plus

--- EvilCorpBanking/src/TransactionList.java
+++ EvilCorpBanking/src/TransactionList.java
@@ -31,21 +31,27 @@ public class TransactionList {
 				
 				// If the dates are the same, compare the transaction types and amounts
 				if (transactionList.get(i).getDate().compareTo(transactionList.get(j).getDate()) == 0) {
-					// If the second transaction type is deposit and the first one is any of the other types, keep the same order
-					if ((!transactionList.get(i).getType().equals("deposit")) && transactionList.get(j).getType().equals("deposit")) {
-						continue;
-					
-					// if the first transaction type is deposit and the second one is any of the other types, swap the order
-					} else if (transactionList.get(i).getType().equals("deposit") && (!transactionList.get(j).getType().equals("deposit"))){
+					// If the second transaction type is "add an account", swap the order
+					if (transactionList.get(j).getType().equals("add an account")) {
 						Transaction temp1 = transactionList.get(i);
 						transactionList.set(i, transactionList.get(j));
 						transactionList.set(j, temp1);
 						
-					// If both of the transactions are not deposit, put the one with larger amount first
-					} else if (transactionList.get(i).getAmount() < transactionList.get(j).getAmount()) {
+					// If the second transaction type is "deposit" and the first one is any of the other types, keep the same order
+					} else if ((!transactionList.get(i).getType().equals("deposit")) && transactionList.get(j).getType().equals("deposit")) {
+						continue;
+					
+					// if the first transaction type is "deposit" and the second one is any of the other types, swap the order
+					} else if (transactionList.get(i).getType().equals("deposit") && (!transactionList.get(j).getType().equals("deposit"))){
 						Transaction temp2 = transactionList.get(i);
 						transactionList.set(i, transactionList.get(j));
 						transactionList.set(j, temp2);
+						
+					// If both of the transactions are not "deposit", put the one with larger amount first
+					} else if (transactionList.get(i).getAmount() < transactionList.get(j).getAmount()) {
+						Transaction temp3 = transactionList.get(i);
+						transactionList.set(i, transactionList.get(j));
+						transactionList.set(j, temp3);
 					}
 				}
 			}
