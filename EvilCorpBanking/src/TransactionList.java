aa839d2c6fd150d023047c878cbc957c038dded8
alice8347 <alice8347@hotmail.com>
2015-08-10 21:54:00 -0400

    Third Commit
    Correct Bonus

--- EvilCorpBanking/src/TransactionList.java
+++ EvilCorpBanking/src/TransactionList.java
@@ -22,17 +22,23 @@ public class TransactionList {
 	public ArrayList<Transaction> sort() {
 		for (int i = 0; i < transactionList.size(); i++) {
 			for (int j = i; j < transactionList.size(); j++) {
+				if (transactionList.get(i).getDate().compareTo(transactionList.get(j).getDate()) > 0) {
+					Transaction temp3 = transactionList.get(i);
+					transactionList.set(i, transactionList.get(j));
+					transactionList.set(j, temp3);
+				}
 				if (transactionList.get(i).getDate().compareTo(transactionList.get(j).getDate()) == 0) {
-					if (transactionList.get(i).getAmount() < transactionList.get(j).getAmount()) {
+					if ((!transactionList.get(i).getType().equals("deposit")) && transactionList.get(j).getType().equals("deposit")) {
+						continue;
+					} else if (transactionList.get(i).getType().equals("deposit") && (!transactionList.get(j).getType().equals("deposit"))){
 						Transaction temp1 = transactionList.get(i);
 						transactionList.set(i, transactionList.get(j));
 						transactionList.set(j, temp1);
+					} else if (transactionList.get(i).getAmount() < transactionList.get(j).getAmount()) {
+						Transaction temp2 = transactionList.get(i);
+						transactionList.set(i, transactionList.get(j));
+						transactionList.set(j, temp2);
 					}
-				}
-				if (transactionList.get(i).getDate().compareTo(transactionList.get(j).getDate()) > 0) {
-					Transaction temp = transactionList.get(i);
-					transactionList.set(i, transactionList.get(j));
-					transactionList.set(j, temp);
 				}
 			}
 		}
